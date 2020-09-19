3f1f28f0d091e63e86fe1b39e497646c456d396d
Helen <melnikelena6969@gmail.com>
2020-09-19 15:40:17 +0300

    Updates

---
+++ precache-manifest.19c7f1a9ab3e053be9b6b44f52f0fd18.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6200e9a531c4f6e72163d4b81a152904",
+    "url": "/Social_network_ts/index.html"
+  },
+  {
+    "revision": "d1bff3d183cefd8e7ddc",
+    "url": "/Social_network_ts/static/css/main.3c8a56eb.chunk.css"
+  },
+  {
+    "revision": "657a671af76570970bb6",
+    "url": "/Social_network_ts/static/js/2.261811d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Social_network_ts/static/js/2.261811d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1bff3d183cefd8e7ddc",
+    "url": "/Social_network_ts/static/js/main.7850b7ac.chunk.js"
+  },
+  {
+    "revision": "6cf813ea5b9195a46d79",
+    "url": "/Social_network_ts/static/js/runtime-main.69551849.js"
+  },
+  {
+    "revision": "258dc82da7a0714663524ba693514b47",
+    "url": "/Social_network_ts/static/media/cover.258dc82d.jpg"
+  },
+  {
+    "revision": "20594e02fb01024d517c8e0867c78bb3",
+    "url": "/Social_network_ts/static/media/penguin.20594e02.png"
+  },
+  {
+    "revision": "1ab0b265a08484d8d8d7eee8c9af0fdc",
+    "url": "/Social_network_ts/static/media/planet.1ab0b265.png"
+  }
+]);
